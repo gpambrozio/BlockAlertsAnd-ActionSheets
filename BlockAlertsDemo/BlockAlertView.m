4ad567780468dfe015844c14ef890472cfb61400
Barrett Jacobsen <barrett@attainmentcompany.com>
2012-03-14 13:58:44 -0500

    localization changes

--- BlockAlertsDemo/BlockAlertView.m
+++ BlockAlertsDemo/BlockAlertView.m
@@ -52,13 +52,13 @@ static UIFont *buttonFont = nil;
 + (void)showInfoAlertWithTitle:(NSString *)title message:(NSString *)message
 {
     BlockAlertView *alert = [[BlockAlertView alloc] initWithTitle:title message:message];
-    [alert setCancelButtonWithTitle:NSLocalizedString(@"Dismiss", @"") block:nil];
+    [alert setCancelButtonWithTitle:NSLocalizedString(@"Dismiss", nil) block:nil];
     [alert show];
 }
 
 + (void)showErrorAlert:(NSError *)error
 {
-    BlockAlertView *alert = [[BlockAlertView alloc] initWithTitle:NSLocalizedString(@"Operation Failed", @"") message:[NSString stringWithFormat:NSLocalizedString(@"The operation did not complete successfully: %@", nil), error]];
+    BlockAlertView *alert = [[BlockAlertView alloc] initWithTitle:NSLocalizedString(@"Operation Failed", nil) message:[NSString stringWithFormat:NSLocalizedString(@"The operation did not complete successfully: %@", nil), error]];
     [alert setCancelButtonWithTitle:@"Dismiss" block:nil];
     [alert show];
 }
