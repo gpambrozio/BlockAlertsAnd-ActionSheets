2d506b3965666bc515bfbc4ca2d5b477ea5ca3c2
Luis Ascorbe <devlascorbe@gmail.com>
2013-01-31 10:53:06 +0100

    Update BlockAlertsDemo/ToAddToYourProjects/BlockAlertView.m
    Enum for animateWithDuration:delay:options:animations: in options isn't the correct one.
    Changed for UIViewAnimationOptionCurveEaseIn and UIViewAnimationOptionCurveEaseOut

--- BlockAlertsDemo/ToAddToYourProjects/BlockAlertView.m
+++ BlockAlertsDemo/ToAddToYourProjects/BlockAlertView.m
@@ -272,7 +272,7 @@ static UIFont *buttonFont = nil;
     
     [UIView animateWithDuration:0.4
                           delay:0.0
-                        options:UIViewAnimationCurveEaseOut
+                        options:UIViewAnimationOptionCurveEaseOut
                      animations:^{
                          [BlockBackground sharedInstance].alpha = 1.0f;
                          _view.center = center;
@@ -317,7 +317,7 @@ static UIFont *buttonFont = nil;
                          completion:^(BOOL finished) {
                              [UIView animateWithDuration:0.4
                                                    delay:0.0 
-                                                 options:UIViewAnimationCurveEaseIn
+                                                 options:UIViewAnimationOptionCurveEaseIn
                                               animations:^{
                                                   CGRect frame = _view.frame;
                                                   frame.origin.y = -frame.size.height;
