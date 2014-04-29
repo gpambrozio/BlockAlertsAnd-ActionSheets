811c1b50315a42078ab865ec810fa24f0916da6a
Josip Cavar <josipcavar04@gmail.com>
2014-04-29 11:45:06 +0200

    Fixed warrnings
    When deployment target is 7.0 there are warnings in project because
    sizeWithFont methods are deprecated.

--- BlockAlertsDemo/ToAddToYourProjects/BlockAlertView.m
+++ BlockAlertsDemo/ToAddToYourProjects/BlockAlertView.m
@@ -65,9 +65,19 @@ static UIFont *buttonFont = nil;
 - (void)addComponents:(CGRect)frame {
     if (_title)
     {
-        CGSize size = [_title sizeWithFont:titleFont
-                         constrainedToSize:CGSizeMake(frame.size.width-kAlertViewBorder*2, 1000)
-                             lineBreakMode:NSLineBreakByWordWrapping];
+        CGSize size;
+        if (IOS_LESS_THAN_7) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+            size = [_title sizeWithFont:titleFont constrainedToSize:CGSizeMake(frame.size.width-kAlertViewBorder*2, 1000) lineBreakMode:NSLineBreakByWordWrapping];
+#pragma clang diagnostic pop
+        }
+        else {
+            NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+            paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
+            size = [_title boundingRectWithSize:CGSizeMake(frame.size.width-kAlertViewBorder*2, 1000) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSParagraphStyleAttributeName : paragraphStyle, NSFontAttributeName : titleFont} context:nil].size;
+            size = CGSizeMake(ceilf(size.width), ceilf(size.height));
+        }
         
         UILabel *labelView = [[UILabel alloc] initWithFrame:CGRectMake(kAlertViewBorder, _height, frame.size.width-kAlertViewBorder*2, size.height)];
         labelView.font = titleFont;
@@ -87,9 +97,19 @@ static UIFont *buttonFont = nil;
     
     if (_message)
     {
-        CGSize size = [_message sizeWithFont:messageFont
-                           constrainedToSize:CGSizeMake(frame.size.width-kAlertViewBorder*2, 1000)
-                               lineBreakMode:NSLineBreakByWordWrapping];
+        CGSize size;
+        if (IOS_LESS_THAN_7) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+            size = [_message sizeWithFont:messageFont constrainedToSize:CGSizeMake(frame.size.width-kAlertViewBorder*2, 1000) lineBreakMode:NSLineBreakByWordWrapping];
+#pragma clang diagnostic pop
+        }
+        else {
+            NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+            paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
+            size = [_message boundingRectWithSize:CGSizeMake(frame.size.width-kAlertViewBorder*2, 1000) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSParagraphStyleAttributeName : paragraphStyle, NSFontAttributeName : messageFont} context:nil].size;
+            size = CGSizeMake(ceilf(size.width), ceilf(size.height));
+        }
         
         UILabel *labelView = [[UILabel alloc] initWithFrame:CGRectMake(kAlertViewBorder, _height, frame.size.width-kAlertViewBorder*2, size.height)];
         labelView.font = messageFont;
@@ -241,22 +261,37 @@ static UIFont *buttonFont = nil;
         {
             // In this case there's another button.
             // Let's check if they fit on the same line.
-            CGSize size = [title sizeWithFont:buttonFont 
-                                  minFontSize:10 
-                               actualFontSize:nil
-                                     forWidth:_view.bounds.size.width-kAlertViewBorder*2 
-                                lineBreakMode:NSLineBreakByClipping];
+            CGSize size;
+            if (IOS_LESS_THAN_7) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+                size = [title sizeWithFont:buttonFont minFontSize:10 actualFontSize:nil forWidth:_view.bounds.size.width-kAlertViewBorder*2 lineBreakMode:NSLineBreakByClipping];
+#pragma clang diagnostic pop
+            }
+            else {
+                NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+                paragraphStyle.lineBreakMode = NSLineBreakByClipping;
+                size = [title boundingRectWithSize:CGSizeMake(_view.bounds.size.width-kAlertViewBorder*2, _height) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSParagraphStyleAttributeName : paragraphStyle,NSFontAttributeName : buttonFont} context:nil].size;
+                size = CGSizeMake(ceilf(size.width), ceilf(size.height));
+            }
             
             if (size.width < maxHalfWidth - kAlertViewBorder)
             {
                 // It might fit. Check the next Button
                 NSArray *block2 = [_blocks objectAtIndex:i+1];
                 NSString *title2 = [block2 objectAtIndex:1];
-                size = [title2 sizeWithFont:buttonFont 
-                                minFontSize:10 
-                             actualFontSize:nil
-                                   forWidth:_view.bounds.size.width-kAlertViewBorder*2 
-                              lineBreakMode:NSLineBreakByClipping];
+                if (IOS_LESS_THAN_7) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+                    size = [title2 sizeWithFont:buttonFont minFontSize:10 actualFontSize:nil forWidth:_view.bounds.size.width-kAlertViewBorder*2 lineBreakMode:NSLineBreakByClipping];
+#pragma clang diagnostic pop
+                }
+                else {
+                    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+                    paragraphStyle.lineBreakMode = NSLineBreakByClipping;
+                    size = [title2 boundingRectWithSize:CGSizeMake(_view.bounds.size.width-kAlertViewBorder*2, _height) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSParagraphStyleAttributeName : paragraphStyle, NSFontAttributeName : buttonFont} context:nil].size;
+                    size = CGSizeMake(ceilf(size.width), ceilf(size.height));
+                }
                 
                 if (size.width < maxHalfWidth - kAlertViewBorder)
                 {
@@ -269,11 +304,19 @@ static UIFont *buttonFont = nil;
         else if (_blocks.count  == 1)
         {
             // In this case this is the ony button. We'll size according to the text
-            CGSize size = [title sizeWithFont:buttonFont
-                                  minFontSize:10
-                               actualFontSize:nil
-                                     forWidth:_view.bounds.size.width-kAlertViewBorder*2
-                                lineBreakMode:NSLineBreakByClipping];
+            CGSize size;
+            if (IOS_LESS_THAN_7) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+                size = [title sizeWithFont:buttonFont minFontSize:10 actualFontSize:nil forWidth:_view.bounds.size.width-kAlertViewBorder*2 lineBreakMode:NSLineBreakByClipping];
+#pragma clang diagnostic pop
+            }
+            else {
+                NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+                paragraphStyle.lineBreakMode = NSLineBreakByClipping;
+                size = [title boundingRectWithSize:CGSizeMake(_view.bounds.size.width-kAlertViewBorder*2, _height) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSParagraphStyleAttributeName : paragraphStyle, NSFontAttributeName : buttonFont} context:nil].size;
+                size = CGSizeMake(ceilf(size.width), ceilf(size.height));
+            }
             
             size.width = MAX(size.width, 80);
             if (size.width + 2 * kAlertViewBorder < width)
@@ -287,10 +330,10 @@ static UIFont *buttonFont = nil;
         button.frame = CGRectMake(xOffset, _height, width, kAlertButtonHeight);
         button.titleLabel.font = buttonFont;
         if (IOS_LESS_THAN_6) {
-#pragma clan diagnostic push
+#pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
             button.titleLabel.minimumFontSize = 10;
-#pragma clan diagnostic pop
+#pragma clang diagnostic pop
         }
         else {
             button.titleLabel.adjustsFontSizeToFitWidth = YES;
