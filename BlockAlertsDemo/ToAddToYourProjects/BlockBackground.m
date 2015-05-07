ed791869cabe1971dbfabc9e0baaae3fd6545585
taymer <tragazzini@gmail.com>
2015-05-07 10:01:01 +0200

    Update BlockBackground.m

--- BlockAlertsDemo/ToAddToYourProjects/BlockBackground.m
+++ BlockAlertsDemo/ToAddToYourProjects/BlockBackground.m
@@ -15,212 +15,246 @@
 
 static BlockBackground *_sharedInstance = nil;
 
-+ (BlockBackground *)sharedInstance {
-  if (_sharedInstance != nil) {
++ (BlockBackground*)sharedInstance
+{
+    if (_sharedInstance != nil) {
+        return _sharedInstance;
+    }
+
+    @synchronized(self) {
+        if (_sharedInstance == nil) {
+            _sharedInstance = [[self alloc] init];
+        }
+    }
+    
     return _sharedInstance;
-  }
+}
 
-  @synchronized(self) {
-    if (_sharedInstance == nil) {
-      _sharedInstance = [[self alloc] init];
++ (id)allocWithZone:(NSZone*)zone
+{
+    @synchronized(self) {
+        if (_sharedInstance == nil) {
+            _sharedInstance = [super allocWithZone:zone];
+            return _sharedInstance;
+        }
     }
-  }
-
-  return _sharedInstance;
+    NSAssert(NO, @ "[BlockBackground alloc] explicitly called on singleton class.");
+    return nil;
 }
 
-+ (id)allocWithZone:(NSZone *)zone {
-  @synchronized(self) {
-    if (_sharedInstance == nil) {
-      _sharedInstance = [super allocWithZone:zone];
-      return _sharedInstance;
+- (id)copyWithZone:(NSZone*)zone
+{
+    return self;
+}
+
+- (id)retain
+{
+    return self;
+}
+
+- (unsigned)retainCount
+{
+    return UINT_MAX;
+}
+
+- (oneway void)release
+{
+}
+
+- (id)autorelease
+{
+    return self;
+}
+
+- (void)setRotation:(NSNotification*)notification
+{
+    if ([[[UIDevice currentDevice] systemVersion] compare:@"8.0" options:NSNumericSearch] == NSOrderedDescending){
+        return;
     }
-  }
-  NSAssert(NO, @"[BlockBackground alloc] explicitly called on singleton class.");
-  return nil;
-}
-
-- (id)copyWithZone:(NSZone *)zone {
-  return self;
-}
-
-- (id)retain {
-  return self;
-}
-
-- (unsigned)retainCount {
-  return UINT_MAX;
-}
-
-- (oneway void)release {
-}
-
-- (id)autorelease {
-  return self;
-}
-
-- (void)setRotation:(NSNotification *)notification {
-  UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
-
-  CGRect orientationFrame = [UIScreen mainScreen].bounds;
-
-  if ((UIInterfaceOrientationIsLandscape(orientation) && orientationFrame.size.height > orientationFrame.size.width) ||
-      (UIInterfaceOrientationIsPortrait(orientation) && orientationFrame.size.width > orientationFrame.size.height)) {
-    float temp = orientationFrame.size.width;
-    orientationFrame.size.width = orientationFrame.size.height;
-    orientationFrame.size.height = temp;
-  }
-
-  self.transform = CGAffineTransformIdentity;
-  self.frame = orientationFrame;
-
-  CGFloat posY = orientationFrame.size.height / 2;
-  CGFloat posX = orientationFrame.size.width / 2;
-
-  CGPoint newCenter;
-  CGFloat rotateAngle;
-
-  switch (orientation) {
-    case UIInterfaceOrientationPortraitUpsideDown:
-      rotateAngle = M_PI;
-      newCenter = CGPointMake(posX, orientationFrame.size.height - posY);
-      break;
-    case UIInterfaceOrientationLandscapeLeft:
-      rotateAngle = -M_PI / 2.0f;
-      newCenter = CGPointMake(posY, posX);
-      break;
-    case UIInterfaceOrientationLandscapeRight:
-      rotateAngle = M_PI / 2.0f;
-      newCenter = CGPointMake(orientationFrame.size.height - posY, posX);
-      break;
-    default:  // UIInterfaceOrientationPortrait
-      rotateAngle = 0.0;
-      newCenter = CGPointMake(posX, posY);
-      break;
-  }
-
-  if ([[[UIDevice currentDevice] systemVersion] compare:@"8.0" options:NSNumericSearch] == NSOrderedDescending) {
-    if (UIInterfaceOrientationIsLandscape(orientation)) {
-      newCenter = CGPointMake(newCenter.y, newCenter.x);
+    
+    UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
+    
+    CGRect orientationFrame = [UIScreen mainScreen].bounds;
+    
+    if(
+       (UIInterfaceOrientationIsLandscape(orientation) && orientationFrame.size.height > orientationFrame.size.width) ||
+       (UIInterfaceOrientationIsPortrait(orientation) && orientationFrame.size.width > orientationFrame.size.height)
+       ) {
+        float temp = orientationFrame.size.width;
+        orientationFrame.size.width = orientationFrame.size.height;
+        orientationFrame.size.height = temp;
     }
-    rotateAngle = 0;
-  }
-
-  self.transform = CGAffineTransformMakeRotation(rotateAngle);
-  self.center = newCenter;
-
-  [self setNeedsLayout];
-  [self layoutSubviews];
+    
+    self.transform = CGAffineTransformIdentity;
+    self.frame = orientationFrame;
+    
+    CGFloat posY = orientationFrame.size.height/2;
+    CGFloat posX = orientationFrame.size.width/2;
+    
+    CGPoint newCenter;
+    CGFloat rotateAngle;
+    
+    switch (orientation) {
+        case UIInterfaceOrientationPortraitUpsideDown:
+            rotateAngle = M_PI;
+            newCenter = CGPointMake(posX, orientationFrame.size.height-posY);
+            break;
+        case UIInterfaceOrientationLandscapeLeft:
+            rotateAngle = -M_PI/2.0f;
+            newCenter = CGPointMake(posY, posX);
+            break;
+        case UIInterfaceOrientationLandscapeRight:
+            rotateAngle = M_PI/2.0f;
+            newCenter = CGPointMake(orientationFrame.size.height-posY, posX);
+            break;
+        default: // UIInterfaceOrientationPortrait
+            rotateAngle = 0.0;
+            newCenter = CGPointMake(posX, posY);
+            break;
+    }
+    
+    if ([[[UIDevice currentDevice] systemVersion] compare:@"8.0" options:NSNumericSearch] == NSOrderedDescending){
+        if(UIInterfaceOrientationIsLandscape(orientation)) {
+            newCenter = CGPointMake(newCenter.y, newCenter.x);
+        }
+        rotateAngle = 0;
+    }
+    
+    self.transform = CGAffineTransformMakeRotation(rotateAngle);
+    self.center = newCenter;
+    
+    [self setNeedsLayout];
+    [self layoutSubviews];
 }
 
-- (id)init {
-  self = [super initWithFrame:[[UIScreen mainScreen] bounds]];
-  if (self) {
-    self.windowLevel = UIWindowLevelStatusBar;
-    self.hidden = YES;
-    self.userInteractionEnabled = NO;
-    self.backgroundColor = [UIColor colorWithWhite:0.4 alpha:0.5f];
-    self.vignetteBackground = NO;
+- (id)init
+{
+    self = [super initWithFrame:[[UIScreen mainScreen] bounds]];
+    if (self) {
+        self.windowLevel = UIWindowLevelStatusBar;
+        self.hidden = YES;
+        self.userInteractionEnabled = NO;
+        self.backgroundColor = [UIColor colorWithWhite:0.4 alpha:0.5f];
+        self.vignetteBackground = NO;
+        
+        if ([[[UIDevice currentDevice] systemVersion] compare:@"8.0" options:NSNumericSearch] == NSOrderedDescending){
+            UIViewController *temp = [UIViewController new];
+            self.rootViewController = temp;
+        }
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(setRotation:)
+                                                     name:UIApplicationDidChangeStatusBarOrientationNotification
+                                                   object:nil];
+        [self setRotation:nil];
 
-    if ([[[UIDevice currentDevice] systemVersion] compare:@"8.0" options:NSNumericSearch] == NSOrderedDescending) {
-      UIViewController *temp = [UIViewController new];
-      self.rootViewController = temp;
     }
+    return self;
+}
 
-    [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(setRotation:)
-                                                 name:UIApplicationDidChangeStatusBarOrientationNotification
-                                               object:nil];
+- (void)addToMainWindow:(UIView *)view
+{
     [self setRotation:nil];
-  }
-  return self;
-}
+    
+    if ([self.subviews containsObject:view]) return;
 
-- (void)addToMainWindow:(UIView *)view {
-  [self setRotation:nil];
-
-  if ([self.subviews containsObject:view]) return;
-
-  if (self.hidden) {
-    _previousKeyWindow = [[[UIApplication sharedApplication] keyWindow] retain];
-    self.alpha = 0.0f;
-    self.hidden = NO;
-    [self makeKeyWindow];
-  }
-
-  // if something's been added to this window, then this window should have interaction
-  self.userInteractionEnabled = YES;
-
-  if (self.subviews.count > 0) {
-    ((UIView *)[self.subviews lastObject]).userInteractionEnabled = NO;
-  }
-
-  if (_backgroundImage) {
-    UIImageView *backgroundView = [[UIImageView alloc] initWithImage:_backgroundImage];
-    backgroundView.frame = self.bounds;
-    backgroundView.contentMode = UIViewContentModeScaleToFill;
-    [self addSubview:backgroundView];
-    [backgroundView release];
-    [_backgroundImage release];
-    _backgroundImage = nil;
-  }
-
-  [self addSubview:view];
-}
-
-- (void)reduceAlphaIfEmpty {
-  NSMutableArray *remainingViews = NSMutableArray.new;
-
-  for (UIView *view in self.subviews) {
-    if ([view isKindOfClass:[UIImageView class]]) {
-      // do nothing, this is the background
-    } else if (CGRectEqualToRect(view.bounds, self.bounds)) {
-      // also do nothing, this is a different type of background
-    } else {
-      [remainingViews addObject:view];
+    if (self.hidden)
+    {
+        _previousKeyWindow = [[[UIApplication sharedApplication] keyWindow] retain];
+        self.alpha = 0.0f;
+        self.hidden = NO;
+        [self makeKeyWindow];
     }
-  }
-
-  if (remainingViews.count == 1) {
-    self.alpha = 0.0f;
-    self.userInteractionEnabled = NO;
-  }
+    
+    // if something's been added to this window, then this window should have interaction
+    self.userInteractionEnabled = YES;
+    
+    if (self.subviews.count > 0)
+    {
+        ((UIView*)[self.subviews lastObject]).userInteractionEnabled = NO;
+    }
+    
+    if (_backgroundImage)
+    {
+        UIImageView *backgroundView = [[UIImageView alloc] initWithImage:_backgroundImage];
+        backgroundView.frame = self.bounds;
+        backgroundView.contentMode = UIViewContentModeScaleToFill;
+        [self addSubview:backgroundView];
+        [backgroundView release];
+        [_backgroundImage release];
+        _backgroundImage = nil;
+    }
+    
+    [self addSubview:view];
 }
 
-- (void)removeView:(UIView *)view {
-  [view removeFromSuperview];
+- (void)reduceAlphaIfEmpty
+{
+    NSMutableArray *remainingViews = NSMutableArray.new;
 
-  UIView *topView = [self.subviews lastObject];
-  if ([topView isKindOfClass:[UIImageView class]]) {
-    // It's a background. Remove it too
-    [topView removeFromSuperview];
-  }
-
-  if (self.subviews.count == 0) {
-    self.hidden = YES;
-    [_previousKeyWindow makeKeyWindow];
-    [_previousKeyWindow release];
-    _previousKeyWindow = nil;
-  } else {
-    ((UIView *)[self.subviews lastObject]).userInteractionEnabled = YES;
-  }
+    for (UIView *view in self.subviews) {
+        if ([view isKindOfClass:[UIImageView class]]) {
+            // do nothing, this is the background
+        }
+        else if (CGRectEqualToRect(view.bounds, self.bounds)) {
+            // also do nothing, this is a different type of background
+        }
+        else {
+            [remainingViews addObject:view];
+        }
+    }
+    
+    
+    if (remainingViews.count == 1)
+    {
+        self.alpha = 0.0f;
+        self.userInteractionEnabled = NO;
+    }
 }
 
-- (void)drawRect:(CGRect)rect {
-  if (_backgroundImage || !_vignetteBackground) return;
-  CGContextRef context = UIGraphicsGetCurrentContext();
+- (void)removeView:(UIView *)view
+{
+    [view removeFromSuperview];
 
-  size_t locationsCount = 2;
-  CGFloat locations[2] = {0.0f, 1.0f};
-  CGFloat colors[8] = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.75f};
-  CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-  CGGradientRef gradient = CGGradientCreateWithColorComponents(colorSpace, colors, locations, locationsCount);
-  CGColorSpaceRelease(colorSpace);
-
-  CGPoint center = CGPointMake(self.bounds.size.width / 2, self.bounds.size.height / 2);
-  float radius = MIN(self.bounds.size.width, self.bounds.size.height);
-  CGContextDrawRadialGradient(context, gradient, center, 0, center, radius, kCGGradientDrawsAfterEndLocation);
-  CGGradientRelease(gradient);
+    UIView *topView = [self.subviews lastObject];
+    if ([topView isKindOfClass:[UIImageView class]])
+    {
+        // It's a background. Remove it too
+        [topView removeFromSuperview];
+    }
+    
+    if (self.subviews.count == 0)
+    {
+        self.hidden = YES;
+        [_previousKeyWindow makeKeyWindow];
+        [_previousKeyWindow release];
+        _previousKeyWindow = nil;
+    }
+    else
+    {
+        ((UIView*)[self.subviews lastObject]).userInteractionEnabled = YES;
+    }
 }
+
+- (void)drawRect:(CGRect)rect 
+{    
+    if (_backgroundImage || !_vignetteBackground) return;
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+	size_t locationsCount = 2;
+	CGFloat locations[2] = {0.0f, 1.0f};
+	CGFloat colors[8] = {0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.75f}; 
+	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+	CGGradientRef gradient = CGGradientCreateWithColorComponents(colorSpace, colors, locations, locationsCount);
+	CGColorSpaceRelease(colorSpace);
+	
+	CGPoint center = CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2);
+	float radius = MIN(self.bounds.size.width , self.bounds.size.height) ;
+	CGContextDrawRadialGradient (context, gradient, center, 0, center, radius, kCGGradientDrawsAfterEndLocation);
+	CGGradientRelease(gradient);
+}
+
+
+
 
 @end
