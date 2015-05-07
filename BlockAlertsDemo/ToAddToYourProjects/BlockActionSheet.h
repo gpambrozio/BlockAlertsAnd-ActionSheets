1dac714987f3d8a9982defc31d93cb09ea411e27
taymer <tragazzini@gmail.com>
2015-05-07 13:07:52 +0200

    Added all in one line buttons and modified colors

--- BlockAlertsDemo/ToAddToYourProjects/BlockActionSheet.h
+++ BlockAlertsDemo/ToAddToYourProjects/BlockActionSheet.h
@@ -9,26 +9,27 @@
  * A simple block-enabled API wrapper on top of UIActionSheet.
  */
 @interface BlockActionSheet : NSObject {
-@private
-    UIView *_view;
-    NSMutableArray *_blocks;
-    CGFloat _height;
+ @private
+  UIView *_view;
+  NSMutableArray *_blocks;
+  CGFloat _height;
+  UIColor *_tintColor;
 }
 
-@property (nonatomic, readonly) UIView *view;
-@property (nonatomic, readwrite) BOOL vignetteBackground;
+@property(nonatomic, readonly) UIView *view;
+@property(nonatomic, readwrite) BOOL vignetteBackground;
 
 + (id)sheetWithTitle:(NSString *)title;
++ (id)sheetWithTitle:(NSString *)title tintColor:(UIColor *)tintColor textColor:(UIColor *)textColor;
+- (id)initWithTitle:(NSString *)title tintColor:(UIColor *)tintColor textColor:(UIColor *)textColor;
 
-- (id)initWithTitle:(NSString *)title;
+- (void)setCancelButtonWithTitle:(NSString *)title block:(void (^)())block;
+- (void)setDestructiveButtonWithTitle:(NSString *)title block:(void (^)())block;
+- (void)addButtonWithTitle:(NSString *)title block:(void (^)())block;
 
-- (void)setCancelButtonWithTitle:(NSString *) title block:(void (^)()) block;
-- (void)setDestructiveButtonWithTitle:(NSString *) title block:(void (^)()) block;
-- (void)addButtonWithTitle:(NSString *) title block:(void (^)()) block;
-
-- (void)setCancelButtonWithTitle:(NSString *) title atIndex:(NSInteger)index block:(void (^)()) block;
-- (void)setDestructiveButtonWithTitle:(NSString *) title atIndex:(NSInteger)index block:(void (^)()) block;
-- (void)addButtonWithTitle:(NSString *) title atIndex:(NSInteger)index block:(void (^)()) block;
+- (void)setCancelButtonWithTitle:(NSString *)title atIndex:(NSInteger)index block:(void (^)())block;
+- (void)setDestructiveButtonWithTitle:(NSString *)title atIndex:(NSInteger)index block:(void (^)())block;
+- (void)addButtonWithTitle:(NSString *)title atIndex:(NSInteger)index block:(void (^)())block;
 
 - (void)showInView:(UIView *)view;
 
